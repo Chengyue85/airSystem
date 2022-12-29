961a5c3bb49a0f5407c8496c526fbff34ac1ed06
Chengyue85 <93128070+Chengyue85@users.noreply.github.com>
2022-12-29 09:49:53 +0800

    Eureka注册

---
+++ AirSystem/airsystem_cloud/src/test/java/com/example/airsystem_cloud/AirsystemCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airsystem_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirsystemCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
